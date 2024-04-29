30aefbafd78e2c94c30f914f46d0da4e2cf4a00f
Stephen01s <stephenrs@byui.edu>
2024-04-29 10:15:51 -0600

    wrote prep 1 and 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,45 @@
 using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("what is your grade percentage? ");
+        string Grade_P = Console.ReadLine();
+        int Grade = int.Parse(Grade_P);
+        string Letter = "";
+        if (Grade >= 90)
+        {
+            Letter = "A";
+        }
+        else if (Grade >= 80)
+        {
+            Letter = "B";
+        }
+        else if (Grade >= 70)
+        {
+            Letter = "C";
+        }
+        else if (Grade >= 60)
+        {
+            Letter = "D";
+        
+        }
+        else
+        {
+            Letter = "F";
+            
+        }
+
+        if (Grade >= 70)
+        {
+            Console.WriteLine("Congrats you pass! ");
+        }
+        else 
+        {
+            Console.WriteLine("You failed");
+        }
     }
 }
