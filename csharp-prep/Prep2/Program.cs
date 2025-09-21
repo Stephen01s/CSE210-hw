109d5180f7ad46c1d43418edf1cba2bbde26cee8
Stephen01s <stephenrs@byui.edu>
2025-09-20 21:46:36 -0600

    Wrote Prep2 Grade Calculator

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your Grade percentage? ");
+        string GradeR = Console.ReadLine();
+        int Grade = int.Parse(GradeR);
+        string letter;
+        if (Grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (Grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (Grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (Grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your Grade is an {letter}");
+        
+        if (Grade >= 70)
+        {
+            Console.WriteLine("You are currently passing, Congrats!");
+        }
+        else
+        {
+            Console.WriteLine("You Dummy, Do better.");
+        }
     }
 }
