814e23337782396cc325177f78d647bfc2a2ae94
Stephen01s <stephenrs@byui.edu>
2025-09-20 21:14:48 -0600

    Wrote program: Bond name

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("What is your first name? ");
+        string FirstN = Console.ReadLine();
+        Console.WriteLine("What is your last name? ");
+        string LastN = Console.ReadLine();
+        Console.WriteLine($"Your name is {LastN}, {FirstN} {LastN}.");
     }
 }
