9b8a9dd2e1f4efff6c0f987eb08864ba43dc0051
Stephen01s <stephenrs@byui.edu>
2025-09-22 12:25:47 -0600

    Wrote Prep3 Number Guessing Game

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magic = randomGenerator.Next(1, 101);
+        int guess;
+        string guessS;
+        do
+        {
+            Console.WriteLine("What is your Guess?");
+            guessS = Console.ReadLine();
+            guess = int.Parse(guessS);
+            if (guess > magic)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess < magic)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("Correct, that was the magic number!");
+            }
+        } while (magic != guess);
+
     }
 }
