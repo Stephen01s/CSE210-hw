627e3b2b7cb1ad0f6901b245ce2c0745464b46ac
Stephen01s <stephenrs@byui.edu>
2025-09-22 15:47:53 -0600

    Wrote Prep4 Number list

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,40 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        float x;
+        float totalsum = 0;
+        float LargestN = 0;
+        List<float> numbers = new List<float>();
+        Console.WriteLine("please enter items of a list. Type 0 when done");
+        do
+        {
+            string y = Console.ReadLine();
+            x = float.Parse(y);
+            if (x != 0)
+            {
+                numbers.Add(x);
+                totalsum += x;
+                if (LargestN < x)
+                {
+                    LargestN = x;
+                }
+            }
+            else
+            {
+
+            }
+        } while (x != 0);
+        // foreach (float number in numbers)
+        // {
+        //     Console.WriteLine(number);
+        // }
+        Console.WriteLine($"the sum is {totalsum}");
+        Console.WriteLine($"the average is {totalsum / numbers.Count}");
+        Console.WriteLine($"the largest number is {LargestN}");
     }
 }
